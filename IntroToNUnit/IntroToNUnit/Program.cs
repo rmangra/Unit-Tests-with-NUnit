0866228b56a6eaa22bd4803dc096db1a75815740
rmangra <richard.mangra@gmail.com>
2018-02-06 23:58:53 -0500

    First commit.  Started new unit test project to review the use of NUnit.

---
+++ IntroToNUnit/IntroToNUnit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace IntroToNUnit
+{
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    */
+    public static class EnumToStringConverter
+    {
+        public static string ToFriendlyString(this Enum value)
+        {
+            var text = value.ToString();
+            var regex = new Regex(@"[\p{Lu}|^\p{Ll}]\p{Ll}*");
+            var split = from Match match in regex.Matches(text) select match.Value.InitialCapital();
+            return string.Join(" ", split);
+        }
+
+        public static string InitialCapital(this string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+                return word;
+
+            return word.First().ToString().ToUpper() + word.Substring(1);
+        }
+    }
+}
